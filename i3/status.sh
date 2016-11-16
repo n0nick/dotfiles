93c3f74b001b81257395932b44b7a37965f7a68b
Sagie Maoz <sagie@google.com>
2016-11-16 14:54:12 +0200

    i3 status: clean up

--- i3/status.sh
+++ i3/status.sh
@@ -5,17 +5,11 @@
 i3status --config ~/.config/i3status/config | while :
 do
     read line
-    HOSTNAME=$(hostname -s)
-    LG=$(setxkbmap -query | awk '/layout/{print $2}') 
 
-    if [ $LG == "il" ]
-    then
-        lang="{ \"full_text\": \"$LG\" }"
-    else
-        lang="{ \"full_text\": \"$LG\" }"
-    fi
-    # dat="[{ \"full_text\": \"$HOSTNAME\" },$lang"
-    dat="[{ \"full_text\": \"$HOSTNAME\" },$lang,"
+    lang="{ \"full_text\": \"$(setxkbmap -query | awk '/layout/{print $2}')\" }"
+    hostname="{ \"full_text\": \"$(hostname -s)\" }"
 
-    echo "${line/[/$dat}" || exit 1
+    dat="$hostname,$lang,"
+
+    echo "${line/[/[$dat}" || exit 1
 done
