fa79d46845a7b87605ca570563c66c715396e288
Sagie Maoz <sagiem@gmail.com>
2026-03-13 10:27:04 -0400

    neovim color scheme

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,6 +1,17 @@
 -- Plugin specifications for lazy.nvim
 
 return {
+  -- Colorscheme
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme('catppuccin-mocha')
+    end,
+  },
+
   -- Editing
   { 'numToStr/Comment.nvim' },
   { 'tpope/vim-repeat' },
