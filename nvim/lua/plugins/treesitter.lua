cc3c90c4ee88adbec77c40e98c4e836860f69be1
Sagie Maoz <sagiem@gmail.com>
2026-03-13 10:17:54 -0400

    nvim-treesitter: updated setup api

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,17 +3,11 @@ return {
   build = ":TSUpdate",
   lazy = false,
   config = function()
-    require("nvim-treesitter").setup({
-      -- Directory to install parsers and queries to
-      install_dir = vim.fn.stdpath('data') .. '/site',
-    })
-
-    -- Install parsers
-    require("nvim-treesitter").install({
-      "csv", "dockerfile", "gitignore", "go", "gomod", "gosum",
-      "gowork", "javascript", "json", "lua", "markdown", "proto",
-      "python", "rego", "sql", "yaml"
+    -- Auto-install parsers on FileType
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function(args)
+        pcall(vim.treesitter.start, args.buf)
+      end,
     })
   end
 }
-
